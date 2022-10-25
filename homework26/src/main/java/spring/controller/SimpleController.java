2d7aee0416895ebb5c9e642dd02fcdd9dfb7bb9e
Alexej Lange <drozdov76@gmail.com>
2022-10-25 20:07:23 +0300

    Homework #26. Final.

--- homework26/src/main/java/spring/controller/SimpleController.java
+++ homework26/src/main/java/spring/controller/SimpleController.java
@@ -1,11 +1,10 @@
 package spring.controller;
 
+import spring.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import spring.service.TestService;
 
 @Controller
 public class SimpleController {
