73d9dbb26590c7ae9b4952823ae4fdf7d8d024d3
Alexej Lange <drozdov76@gmail.com>
2022-12-14 17:46:00 +0100

    Homework #36. Repeat lessons code.

---
+++ homework36/src/main/java/spring/SimpleWebApp.java
@@ -0,0 +1,20 @@
+package spring;
+
+/**
+ * Spring Boot (Spring Boot + h2 database + tests)
+ * @author Alexej Lange
+ * @version 13 Dec 2022
+ *
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApp.class, args);
+	}
+}
+
